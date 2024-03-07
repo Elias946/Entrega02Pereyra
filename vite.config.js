b19a8ab04fe8a3f5caf9c686cd0b04c3c7b9d467
Elias946 <43368837+Elias946@users.noreply.github.com>
2024-03-07 00:21:06 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://elias946.github.io/Entrega02Pereyra/'
 })
